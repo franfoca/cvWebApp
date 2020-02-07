7e29fcb1211a97739df95328211cf7be11e35890
Francisco Focaraccio <francisco_focaraccio@hotmail.com>
2020-02-07 16:42:12 -0300

    Initial Structure of web app

---
+++ cvWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace cvWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
